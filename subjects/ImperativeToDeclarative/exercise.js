36824a5df12631a5c54ae7c9d48f980a37ba9b7e
Kyle Kelley <rgbkrk@gmail.com>
2017-09-21 10:10:20 -0700

    complete modals exercise

--- subjects/ImperativeToDeclarative/exercise.js
+++ subjects/ImperativeToDeclarative/exercise.js
@@ -4,77 +4,98 @@
 // This Modal, even though its a React component, has an imperative API to
 // open and close it. Can you convert it to a declarative API?
 ////////////////////////////////////////////////////////////////////////////////
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-import $ from 'jquery'
-import 'bootstrap-webpack'
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import $ from "jquery";
+import "bootstrap-webpack";
 
 class Modal extends React.Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
     children: PropTypes.node
+  };
+
+  componentDidMount() {
+    if (this.props.open) {
+      this.open();
+    } else {
+      this.close();
+    }
   }
 
-  open() {
-    $(this.node).modal('show')
+  componentDidUpdate() {
+    if (this.props.open) {
+      this.open();
+    } else {
+      this.close();
+    }
   }
 
-  close() {
-    $(this.node).modal('hide')
-  }
+  open = () => {
+    $(this.node).modal("show");
+  };
+
+  close = () => {
+    $(this.node).modal("hide");
+  };
 
   render() {
     return (
-      <div className="modal fade" ref={node => this.node = node}>
+      <div className="modal fade" ref={node => (this.node = node)}>
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
               <h4 className="modal-title">{this.props.title}</h4>
             </div>
-            <div className="modal-body">
-              {this.props.children}
-            </div>
+            <div className="modal-body">{this.props.children}</div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 class App extends React.Component {
   openModal = () => {
-    this.modal.open()
-  }
+    this.setState({ modalOpen: true });
+  };
 
   closeModal = () => {
-    this.modal.close()
-  }
+    this.setState({ modalOpen: false });
+  };
+
+  state = {
+    modalOpen: false
+  };
 
   render() {
     return (
       <div className="container">
-        <h1>Let’s make bootstrap modal declarative</h1>
+        <h1>Let’s make bootstrap modal DECLARATIVE</h1>
 
-        <button
-          className="btn btn-primary"
-          onClick={this.openModal}
-        >open modal</button>
+        <button className="btn btn-primary" onClick={this.openModal}>
+          open modal
+        </button>
 
-        <Modal title="Declarative is better" ref={modal => this.modal = modal}>
+        <Modal title="Declarative is better" open={this.state.modalOpen}>
           <p>Calling methods on instances is a FLOW not a STOCK!</p>
           <p>It’s the dynamic process, not the static program in text space.</p>
-          <p>You have to experience it over time, rather than in snapshots of state.</p>
+          <p>
+            You have to experience it over time, rather than in snapshots of
+            state.
+          </p>
           <button
             onClick={this.closeModal}
             type="button"
             className="btn btn-default"
-          >Close</button>
+          >
+            Close
+          </button>
         </Modal>
-
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
