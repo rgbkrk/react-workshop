4515dfebf433334c2f2282b1c56e812522eecef9
Kyle Kelley <rgbkrk@gmail.com>
2017-09-21 15:21:51 -0700

    [lecture] demonstrate context and limitations

--- subjects/Context/lecture.js
+++ subjects/Context/lecture.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-import * as styles from './styles'
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import * as styles from "./styles";
 
 ////////////////////////////////////////////////////////////////////////////////
 // Sometimes you don't want to specify how deep in the view tree the child
@@ -14,15 +14,20 @@ import * as styles from './styles'
 // plan, so instead we can use a feature called "context".
 
 class TabList extends React.Component {
+  static contextTypes = {
+    activeIndex: PropTypes.number.isRequired,
+    activateTab: PropTypes.func.isRequired
+  };
+
   render() {
     const children = React.Children.map(this.props.children, (child, index) => {
       return React.cloneElement(child, {
-        isActive: index === this.props.activeIndex,
-        onClick: () => this.props.onActivate(index)
-      })
-    })
+        isActive: index === this.context.activeIndex,
+        onClick: () => this.context.activateTab(index)
+      });
+    });
 
-    return <div style={styles.tabs}>{children}</div>
+    return <div style={styles.tabs}>{children}</div>;
   }
 }
 
@@ -31,79 +36,94 @@ class Tab extends React.Component {
     return (
       <div
         onClick={this.props.isDisabled ? null : this.props.onClick}
-        style={this.props.isDisabled ? styles.disabledTab : (
-          this.props.isActive ? styles.activeTab : styles.tab
-        )}
+        style={
+          this.props.isDisabled
+            ? styles.disabledTab
+            : this.props.isActive ? styles.activeTab : styles.tab
+        }
       >
         {this.props.children}
       </div>
-    )
+    );
   }
 }
 
 class TabPanels extends React.Component {
+  static contextTypes = {
+    activeIndex: PropTypes.number.isRequired
+  };
+
   render() {
     return (
       <div style={styles.tabPanels}>
-        {this.props.children[this.props.activeIndex]}
+        {this.props.children[this.context.activeIndex]}
       </div>
-    )
+    );
   }
 }
 
 class TabPanel extends React.Component {
   render() {
-    return <div>{this.props.children}</div>
+    return <div>{this.props.children}</div>;
   }
 }
 
 class Tabs extends React.Component {
   state = {
     activeIndex: 0
+  };
+
+  static childContextTypes = {
+    activeIndex: PropTypes.number,
+    activateTab: PropTypes.func.isRequired
+  };
+
+  getChildContext() {
+    return {
+      activeIndex: this.state.activeIndex,
+      activateTab: index => this.setState({ activeIndex: index })
+    };
   }
 
   render() {
-    const children = React.Children.map(this.props.children, (child, index) => {
-      if (child.type === TabPanels) {
-        return React.cloneElement(child, {
-          activeIndex: this.state.activeIndex
-        })
-      } else if (child.type === TabList) {
-        return React.cloneElement(child, {
-          activeIndex: this.state.activeIndex,
-          onActivate: (activeIndex) => this.setState({ activeIndex })
-        })
-      } else {
-        return child
-      }
-    })
-
-    return <div>{children}</div>
+    return <div>{this.props.children}</div>;
   }
 }
 
 class App extends React.Component {
-  render () {
+  render() {
     return (
       <div>
         <Tabs>
-          <TabList>
-            <Tab>Tacos</Tab>
-            <Tab isDisabled>Burritos</Tab>
-            <Tab>Coconut Korma</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel><p>Tacos are delicious</p></TabPanel>
-            <TabPanel><p>Sometimes a burrito is what you really need</p></TabPanel>
-            <TabPanel><p>Might be your best option</p></TabPanel>
-          </TabPanels>
+          <div style={{ backgroundColor: "#bbbbbb" }}>
+            <UpdateBlocker>
+              <TabList>
+                <Tab>Tacos</Tab>
+                <Tab isDisabled>Burritos</Tab>
+                <Tab>Coconut Korma</Tab>
+              </TabList>
+            </UpdateBlocker>
+          </div>
+          <div style={{ backgroundColor: "#eeeeee" }}>
+            <TabPanels>
+              <TabPanel>
+                <p>Tacos are delicious</p>
+              </TabPanel>
+              <TabPanel>
+                <p>Sometimes a burrito is what you really need</p>
+              </TabPanel>
+              <TabPanel>
+                <p>Might be your best option</p>
+              </TabPanel>
+            </TabPanels>
+          </div>
         </Tabs>
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
 
 ////////////////////////////////////////////////////////////////////////////////
 // Wrapping <TabPanels> in a div breaks everything! Instead of using
