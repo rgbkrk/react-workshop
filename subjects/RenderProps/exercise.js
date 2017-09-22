d568ce9effe4d64ac97fa0325d0cde37b28a1b8f
Kyle Kelley <rgbkrk@gmail.com>
2017-09-22 12:28:57 -0700

    cancel this out for now...

--- subjects/RenderProps/exercise.js
+++ subjects/RenderProps/exercise.js
@@ -18,58 +18,112 @@
 //   needed to render it
 // - Make sure <GeoAddress> supports the user moving positions
 ////////////////////////////////////////////////////////////////////////////////
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-import LoadingDots from './utils/LoadingDots'
-import getAddressFromCoords from './utils/getAddressFromCoords'
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import LoadingDots from "./utils/LoadingDots";
+import getAddressFromCoords from "./utils/getAddressFromCoords";
 
-class App extends React.Component {
+// class GeoAddress extends React.Component {
+//   state = {
+//     address: null,
+//     error: null
+//   };
+//
+//   fetchAddress = () => {
+//     if (this.props.latitude && this.props.longitude) {
+//       getAddressFromCoords(this.props.latitude, this.props.longitude)
+//         .then(address => {
+//           this.setState({ address });
+//         })
+//         .catch(error => {
+//           this.setState({ error });
+//         });
+//     }
+//   };
+//
+//   shouldComponentUpdate(nextProps) {
+//     return (
+//       this.props.latitude !== nextProps.latitude &&
+//       this.props.longitude !== nextProps.longitude
+//     );
+//   }
+//
+//   componentDidMount() {
+//     this.fetchAddress();
+//   }
+//
+//   componentDidUpdate() {
+//     this.fetchAddress();
+//   }
+//
+//   componentWillUnmount() {
+//     this.setState({ address: null, error: null });
+//   }
+//
+//   render() {
+//     return <div>The address is {this.state.address || <LoadingDots />}</div>;
+//   }
+// }
+//
+//
+class GeoPosition extends React.Component {
   state = {
     coords: {
       latitude: null,
       longitude: null
     },
     error: null
-  }
+  };
 
   componentDidMount() {
     this.geoId = navigator.geolocation.watchPosition(
-      (position) => {
+      position => {
         this.setState({
           coords: {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
           }
-        })
+        });
       },
-      (error) => {
-        this.setState({ error })
+      error => {
+        this.setState({ error });
       }
-    )
+    );
   }
 
   componentWillUnmount() {
-    navigator.geolocation.clearWatch(this.geoId)
+    navigator.geolocation.clearWatch(this.geoId);
   }
 
   render() {
-    return (
-      <div>
-        <h1>Geolocation</h1>
-        {this.state.error ? (
-          <div>Error: {this.state.error.message}</div>
-        ) : (
-          <dl>
-            <dt>Latitude</dt>
-            <dd>{this.state.coords.latitude || <LoadingDots/>}</dd>
-            <dt>Longitude</dt>
-            <dd>{this.state.coords.longitude || <LoadingDots/>}</dd>
-          </dl>
-        )}
-      </div>
-    )
+    return this.props.children(this.state);
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'))
+class App extends React.Component {
+  render() {
+    return (
+      <GeoPosition>
+        {geo => (
+          <div>
+            <h1>Geolocation</h1>
+            {geo.error ? (
+              <div>Error: {geo.error.message}</div>
+            ) : (
+              <dl>
+                <dt>Latitude</dt>
+                <dd>{geo.coords.latitude || <LoadingDots />}</dd>
+                <dt>Longitude</dt>
+                <dd>{geo.coords.longitude || <LoadingDots />}</dd>
+              </dl>
+            )}
+            {/* <GeoAddress latitude={geo.latitude} longitude={geo.longitude} /> */}
+          </div>
+        )}
+      </GeoPosition>
+    );
+  }
+}
+
+// ReactDOM.render(<App />, document.getElementById("app"));
