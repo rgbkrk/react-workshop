c2c874c65b9ada8c5b2ccdd1d8198dc482513358
Kyle Kelley <rgbkrk@gmail.com>
2017-09-22 10:23:44 -0700

    finish HoC exercise (and a little bit of extra)

--- subjects/HigherOrderComponents/exercise.js
+++ subjects/HigherOrderComponents/exercise.js
@@ -10,14 +10,34 @@
 //
 // Make a `withCat` HOC that shows a cat chasing the mouse around the screen!
 ////////////////////////////////////////////////////////////////////////////////
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-import * as styles from './styles'
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import * as styles from "./styles";
 
-const withMouse = (Component) => {
-  return Component
-}
+const Cat = props => <div style={styles.cat} />;
+
+const withMouse = Component => {
+  return class extends React.Component {
+    state = {
+      mouse: {
+        x: 0,
+        y: 0
+      }
+    };
+
+    render() {
+      return (
+        <div
+          onMouseMove={event =>
+            this.setState({ mouse: { x: event.clientX, y: event.clientY } })}
+        >
+          <Component {...this.props} mouse={this.state.mouse} />
+        </div>
+      );
+    }
+  };
+};
 
 class App extends React.Component {
   static propTypes = {
@@ -25,23 +45,25 @@ class App extends React.Component {
       x: PropTypes.number.isRequired,
       y: PropTypes.number.isRequired
     }).isRequired
-  }
+  };
 
   render() {
-    const { mouse } = this.props
+    const { mouse, cat } = this.props;
 
     return (
       <div style={styles.container}>
         {mouse ? (
-          <h1>The mouse position is ({mouse.x}, {mouse.y})</h1>
+          <h1>
+            The mouse position is ({mouse.x}, {mouse.y})
+          </h1>
         ) : (
           <h1>We don't know the mouse position yet :(</h1>
         )}
       </div>
-    )
+    );
   }
 }
 
-const AppWithMouse = withMouse(App)
+const AppWithMouse = withMouse(App);
 
-ReactDOM.render(<AppWithMouse/>, document.getElementById('app'))
+ReactDOM.render(<AppWithMouse />, document.getElementById("app"));
